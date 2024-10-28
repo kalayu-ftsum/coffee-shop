ff1e434c5c7a682b35b7b1680e81d33fef153e17
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2024-10-28 15:49:42 +0100

    fixed header style issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,21 +8,21 @@ export default function Header() {
     <nav className='flex justify-between flex-grow items-center'>
         <ul className='flex gap-x-8 text-white'>
             <li>
-                <a href="/"  className='font-butler text-2xl font-normal'>Home</a>
+                <a href="/"  className='font-butler text-xl font-normal'>Home</a>
             </li>
             <li>
-                <a href="#aboutus"  className='font-butler text-2xl font-normal'>About Us</a>
+                <a href="#aboutus"  className='font-butler text-xl font-normal'>About Us</a>
             </li>
             <li>
-                <a href="#menu" className='font-butler text-2xl font-normal'>Menu</a>
+                <a href="#menu" className='font-butler text-xl font-normal'>Menu</a>
             </li>
             <li>
-                <a href="#specials" className='font-butler text-2xl font-normal'>Our Specials</a>
+                <a href="#specials" className='font-butler text-xl font-normal'>Our Specials</a>
             </li>
         </ul>
         <div className='flex gap-x-4'>
-            <button className='font-butler font-bold text-lg px-8 py-4 rounded-lg border border-primary text-primary'>Sign in</button>
-            <button className='font-butler font-bold text-lg px-8 py-4 rounded-lg bg-primary text-white'>Sign up</button>
+            <button className='font-butler  h-10 font-bold text-lg leading-none px-8 rounded-lg border border-primary text-primary'>Sign in</button>
+            <button className='font-butler h-10  font-bold text-lg leading-none px-8 rounded-lg bg-primary text-white'>Sign up</button>
 
         </div>
     </nav>
